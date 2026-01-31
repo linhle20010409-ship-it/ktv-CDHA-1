0751d7663638d6ea1f29fb31da896dbb0f4c10ef
linhle20010409-ship-it <linhle20010409@gmail.com>
2026-01-31 15:34:38 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // DÒNG QUAN TRỌNG NHẤT: Khai báo tên Repository của bạn
-  // Dựa trên link web của bạn là .../ktv-CDHA-1/
+  // ĐÂY LÀ DÒNG QUAN TRỌNG NHẤT
+  // Nó giúp web tìm đúng file trong thư mục dự án của bạn
   base: "/ktv-CDHA-1/",
-  build: {
-    outDir: 'dist',
-  },
-});
+})
